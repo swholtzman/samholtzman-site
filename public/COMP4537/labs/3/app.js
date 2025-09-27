51c759171af3e171b0302f1bef4bc916eb1bf631
Sam Holtzman <Sam.Wolf.Holtzman@gmail.com>
2025-09-27 14:53:04 -0700

    debugging with node.js

---
+++ public/COMP4537/labs/3/app.js
@@ -0,0 +1,5 @@
+"use strict";
+import * as math from './math.js';
+
+console.log(`Hello, Sam. Adding 6 and 7 gives: ${math.addNumbers(6, 7)}`);
+console.log(`Hello, Sam. Subtracting 6 from 7 gives: ${math.subtractNumbers(7, 6)}`);
---
+++ public/COMP4537/labs/3/math.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+export function addNumbers (a, b) {
+    return a + b;
+};
+
+export function subtractNumbers ( a, b ) {
+    return a - b;
+};
